2f8b7a7e3ab19174ba5b329f7a51c73c1cc1cb07
NeroSiegfried <victornabasu@yahoo.com>
2025-04-16 23:10:13 +0100

    Configuring the C compiler

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+
 int main(int argc, char* argv[]) {
     InputBuffer* input_buffer = new_input_buffer();
     while(true) {
@@ -11,4 +14,31 @@ int main(int argc, char* argv[]) {
             printf("Unrecognized command '%s'.\n", input_buffer->buffer);
         }
     }
+}
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
+    input_buffer -> buffer = NULL;
+    input_buffer -> buffer_length = 0;
+    input_buffer -> input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() {
+    printf("db > ");
+}
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read =
+    getline(&(input_buffer -> buffer), &(input_buffer -> buffer_length), stdin);
+    
 }
